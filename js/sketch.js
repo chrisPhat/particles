8f715142b21a050ead9b0da1a0affb36243b97c6
Christopher Booth <christopher.booth@plymouth.ac.uk>
2021-11-01 14:13:09 +0000

    initial commit

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,66 @@
+const particles = [];
+
 function setup() {
-    var canvas = createCanvas(800, 800);
+    var canvas = createCanvas(window.innerWidth, window.innerHeight);
     canvas.parent("p5container");
+
+    const particlesLength = Math.floor(window.innerWidth / 8);
+    for(let i = 0; i < particlesLength; i++){
+        particles.push(new Particle)
+    }
 }
 
 function draw() {
-    background(220);
+    background('rgba(109,104,117, 0.2)');
+    particles.forEach((p, index)=>{
+        p.draw();
+        p.update();
+        p.checkParticles(particles.slice(index));
+    })
+    
+    stroke('rgb(80, 80, 80)');
+    fill('rgba(255,255,255,0.2)');
+    ellipse((width/2)-100, height/2, 80);
+    ellipse(width/2, height/2, 80);
+    ellipse((width/2)+100, height/2, 80);
+}
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+class Particle {
+   constructor(){
+       this.pos = createVector(random(width), random(height));
+       this.vel = createVector(random(-2, 2), random(-2, 2));
+       this.size = 5;
+   }
+
+   update(){
+       this.pos.add(this.vel);
+       this.edges();
+   }
+
+   draw(){
+       noStroke();
+       fill('rgba(255,255,255,0.1)');
+       circle(this.pos.x, this.pos.y, this.size)
+   }
+
+   edges(){
+       if(this.pos.x < 0 || this.pos.x > width){
+        this.vel.x *= -1;
+       }
+
+       if(this.pos.y < 0 || this.pos.y > height){
+        this.vel.y *= -1;
+       }
+   }
+
+   checkParticles(particles){
+    particles.forEach(particle =>{
+        const d = dist(this.pos.x, this.pos.y, particle.pos.x, particle.pos.y);
+
+        if(d < 150){
+            stroke('rgba(255, 255, 255, 0.1)');
+            line(this.pos.x, this.pos.y, particle.pos.x, particle.pos.y);
+        }
+    })
+   }
 }
